fcc077ae2acd607e438a275d855db12c5ee088ab
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-07-25 09:51:32 +0300

    Задания к объектам

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-console.log(1);
-console.log(2);
+const obj = {
+  age: 10,
+  name: 'name',
+};
+
+if ('name' in obj) {
+  console.log(obj.name);
+}
+
+if ('nick' in obj) { // Не сработает, поля нет, проверка даст false
+  console.log(obj.nick);
+}
+
+if ('age' in obj) {
+  console.log(obj.age);
+}
+
+/* Вывод программы:
+name
+10
+*/
