bfc9d7f78aa1ee3032f3cd9c576b887cd75d9297
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-08-06 11:15:03 +0300

    _

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,40 @@
-console.log('Этот файл отредактирован через GitHub');
+type Person = {
+  name: string;
+  child: Person | null;
+};
+
+const person: Person = {
+  name: '1',
+  child: {
+    name: '2',
+    child: {
+      name: '3',
+      child: {
+        name: '4',
+        child: null,
+      },
+    },
+  },
+};
+
+const person2: Person = {
+  name: '1',
+
+  child: {
+    name: '2',
+
+    child: {
+      name: '3',
+
+      child: {
+        name: '4',
+
+        child: {
+          name: '5',
+
+          child: null,
+        },
+      },
+    },
+  },
+};
