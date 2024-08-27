43293339d4d77c11db7cacdd8141dacb511a7f34
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-08-27 09:41:31 +0300

    (goga-2) Добавлено условие 2

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 const criteria1 = true;
+const criteria2 = true;
 
 if (criteria1) {
   console.log('Условие номер 1 успешно выполнено!');
+}
+
+if (criteria2) {
+  console.log('Условие номер 2 успешно выполнено!');
 }
