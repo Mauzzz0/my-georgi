aa02b9ad9616f8a296934dd1f5b3db3ffa2e633c
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-08-27 09:43:45 +0300

    (goga-2) Добавлено условие Георгия

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 const criteria1 = true;
+const gogaCriteria = true;
 
 if (criteria1) {
   console.log('Условие номер 1 успешно выполнено!');
+}
+
+if (gogaCriteria) {
+  console.log('Условие Георгия сработало!');
 }
