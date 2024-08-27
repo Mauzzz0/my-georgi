087e586c08b74c779b30ac023a21fa5ffa420be9
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-08-27 09:40:23 +0300

    Очищен main

--- src/films.ts
+++
@@ -1,6 +0,0 @@
-export const films = () => {
-  console.log('Результат ручного мержа:');
-  console.log(1 + 1);
-  const g = { x: 10 };
-  console.log(g);
-};
--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { books } from './books';
-import { films } from './films';
-import { music } from './music';
-
-films();
-music();
-books();
