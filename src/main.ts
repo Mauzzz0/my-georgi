2e329dcf563864fd0d37375cd3813c1a43dd8fc3
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-09-06 10:36:36 +0300

    Добавлен сервер и пару обработчиков

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-console.log('СЕРВЕР!');
+import express, { Request, Response } from 'express';
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/task/:id', (req: Request, res: Response) => {
+  res.json(req.params);
+});
+
+server.post('/task', (req: Request, res: Response) => {
+  res.json({ a: false });
+});
+
+const port = 2000;
+
+server.listen(port, () => {
+  console.log(`Сервер запущен на порту ${port}`);
+});
